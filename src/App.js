44eae26c83391de797c1dce2cf616ab42380a9c2
Alka <rashinkaralka@gmail.com>
2023-11-21 15:57:34 +0530

    added hash

--- src/App.js
+++ src/App.js
@@ -1,38 +1,40 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
 import Header from "./Header";
 import Homepage from "./Homepage";
 import Footer from "./Footer";
 import EmiCalc from "./EmiCalc";
 import Faqs from "./Faqs";
 import About from "./About";
-
 import TermsCon from "./TermsCon";
 import Privacy from "./Privacy";
 import Contact from "./Contact";
 import Personel from "./Personel";
 import Blog from "./Blog";
 import DuplicateCusLead from "./DuplicateCusLead";
+import CheckDuplicateCustomer from "./Form/CheckDuplicateCustomer";
+import PreApproval from "./Form/PreApproval";
+
 function App() {
   return (
     <div className="main-wrapper">
-    <BrowserRouter>
-      <Header />
-
-      <Routes>
-        <Route exact path="/" element={<Homepage />} />
-        <Route path="/personelloan" element={<Personel />} />
-        <Route path="/emicalculator" element={<EmiCalc />} />
-        <Route path="/faqs" element={<Faqs />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/terms" element={<TermsCon />} />
-        <Route path="/privacy" element={<Privacy />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/duplicate" element={<DuplicateCusLead />} />
-
-      </Routes>
-      <Footer />
-    </BrowserRouter>
+      <HashRouter>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Homepage />} />
+          <Route exact path="/personelloan" element={<Personel />} />
+          <Route path="/emicalculator" element={<EmiCalc />} />
+          <Route path="/faqs" element={<Faqs />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/terms" element={<TermsCon />} />
+          <Route path="/privacy" element={<Privacy />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/duplicate" element={<DuplicateCusLead />} />
+          <Route path="/checkdupcusform" element={<CheckDuplicateCustomer />} />
+          <Route path="/preapproval" element={<PreApproval />} />
+        </Routes>
+        <Footer />
+      </HashRouter>
     </div>
   );
 }
